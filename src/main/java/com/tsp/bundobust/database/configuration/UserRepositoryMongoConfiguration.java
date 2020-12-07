f4b68b160525ab2d2d25551538ab9ebcb7f8ebdc
robosoftuser <robosoft77@gmail.com>
2020-12-06 21:21:00 -0500

    Refactored the code

--- src/main/java/com/tsp/bundobust/database/configuration/UserRepositoryMongoConfiguration.java
+++ src/main/java/com/tsp/bundobust/database/configuration/UserRepositoryMongoConfiguration.java
@@ -1,20 +1,9 @@
-//package com.tsp.bundobust.database.configuration;
-//
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.data.mongodb.core.MongoTemplate;
-//import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-//
-//import com.mongodb.client.MongoClient;
-//
-//@EnableMongoRepositories(basePackages = "com.tsp.bundobust.payload.repository",
-//							mongoTemplateRef = "userDatabaseMongoTemplateRef")
-//public class UserRepositoryMongoConfiguration {
-//
-//	@Bean
-//	public MongoTemplate userDatabaseMongoTemplateRef(@Qualifier("mongo")MongoClient mongoClient) {
-//
-//		return new MongoTemplate(mongoClient, "employeeDatabase");
-//
-//	}
-//}
+package com.tsp.bundobust.database.configuration;
+
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@EnableMongoRepositories(basePackages = "com.tsp.bundobust.payload.repository",
+							mongoTemplateRef = "mongoTemplate")
+public class UserRepositoryMongoConfiguration {
+
+}
