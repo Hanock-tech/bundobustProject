39e1739e0614023261e4436dfd79e27d2d0a4008
robosoftuser <robosoft77@gmail.com>
2021-01-29 16:37:21 -0500

    added api to fetch employee information

--- src/main/java/com/tsp/bundobust/controllers/EmployeeController.java
+++ src/main/java/com/tsp/bundobust/controllers/EmployeeController.java
@@ -1,25 +1,35 @@
 package com.tsp.bundobust.controllers;
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tsp.bundobust.payload.request.UiEmployeeUpdateRequest;
+import com.tsp.bundobust.models.Employee;
+import com.tsp.bundobust.payload.repository.UserRepository;
+import com.tsp.bundobust.response.UIBaseResponse;
 
-@RestController("/api/employee/details")
+@RestController("/api/employee")
 public class EmployeeController {
 	
 	private final Logger log = LoggerFactory.getLogger(EmployeeController.class);
 	
-	@PutMapping(headers="X-Version=1.0", 
+	private UserRepository userRepository;
+	
+	@PutMapping(path="/details", headers="X-Version=1.0", 
 			produces=MediaType.APPLICATION_JSON_VALUE, 
 			consumes=MediaType.APPLICATION_JSON_VALUE)
-	public void updateEmployeeDetails(@RequestBody UiEmployeeUpdateRequest updateRequest) {
-		
-		log.info("updateEmployeeDetails: ");
+	public  ResponseEntity<UIBaseResponse> getEmployeeDetails(@Valid @RequestParam String employeeId) {
+		log.info("getEmployeeDetails: for Id={}", employeeId);
+		Employee employee = userRepository.findById(employeeId).orElse(null);
+		UIBaseResponse baseResponse = new UIBaseResponse();
+		baseResponse.setData(employee);
+		return new ResponseEntity<>(baseResponse, HttpStatus.OK);
 	}
-
 }
