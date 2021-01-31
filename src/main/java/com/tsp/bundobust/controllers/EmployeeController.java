5e58d25192beab6a0d1659d02bfd40a4f1d3b04b
ranjith909 <ranjithrao909@gmail.com>
2021-01-31 22:01:08 +0530

    Updated PoliceStation Api

--- src/main/java/com/tsp/bundobust/controllers/EmployeeController.java
+++ src/main/java/com/tsp/bundobust/controllers/EmployeeController.java
@@ -7,7 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,17 +16,16 @@ import com.tsp.bundobust.models.Employee;
 import com.tsp.bundobust.payload.repository.UserRepository;
 import com.tsp.bundobust.response.UIBaseResponse;
 
-@RestController("/api/employee")
+@RestController
+@RequestMapping("/api/employee")
 public class EmployeeController {
-	
+
 	private final Logger log = LoggerFactory.getLogger(EmployeeController.class);
-	
+
 	private UserRepository userRepository;
-	
-	@PutMapping(path="/details", headers="X-Version=1.0", 
-			produces=MediaType.APPLICATION_JSON_VALUE, 
-			consumes=MediaType.APPLICATION_JSON_VALUE)
-	public  ResponseEntity<UIBaseResponse> getEmployeeDetails(@Valid @RequestParam String employeeId) {
+
+	@GetMapping(path = "/details", headers = "X-Version=1.0", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<UIBaseResponse> getEmployeeDetails(@Valid @RequestParam String employeeId) {
 		log.info("getEmployeeDetails: for Id={}", employeeId);
 		Employee employee = userRepository.findById(employeeId).orElse(null);
 		UIBaseResponse baseResponse = new UIBaseResponse();
