8d3a09185fb380f4c0a95cb8433add281a984e64
robosoftuser <robosoft77@gmail.com>
2021-02-01 22:46:27 -0500

    added api's for police station and event

--- src/main/java/com/tsp/bundobust/controllers/EmployeeController.java
+++ src/main/java/com/tsp/bundobust/controllers/EmployeeController.java
@@ -1,12 +1,16 @@
 package com.tsp.bundobust.controllers;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,14 +26,33 @@ public class EmployeeController {
 
 	private final Logger log = LoggerFactory.getLogger(EmployeeController.class);
 
+	@Autowired
 	private UserRepository userRepository;
 
-	@GetMapping(path = "/details", headers = "X-Version=1.0", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(path = "/details", 
+			headers = "X-Version=1.0", 
+			produces = MediaType.APPLICATION_JSON_VALUE, 
+			consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<UIBaseResponse> getEmployeeDetails(@Valid @RequestParam String employeeId) {
 		log.info("getEmployeeDetails: for Id={}", employeeId);
 		Employee employee = userRepository.findById(employeeId).orElse(null);
 		UIBaseResponse baseResponse = new UIBaseResponse();
 		baseResponse.setData(employee);
+		return new ResponseEntity<>(baseResponse, HttpStatus.OK);
+	}
+	
+	
+	public ResponseEntity<UIBaseResponse> getAllEmployeeDetails(
+			@Valid @RequestParam String policeStationName) {
+		
+		List<Employee> employees = userRepository.findByPoliceStationName(policeStationName);
+		
+		if(CollectionUtils.isEmpty(employees)) {
+			//TODO: Write exception code and message throw new DatabaseException(uiPostingDetailsError, unprocessableEntity)
+		}
+		
+		UIBaseResponse baseResponse = new UIBaseResponse();
+		baseResponse.setData(employees);
 		return new ResponseEntity<>(baseResponse, HttpStatus.OK);
 	}
 }
---
+++ src/main/java/com/tsp/bundobust/controllers/EventDetailsUiRequest.java
@@ -0,0 +1,15 @@
+package com.tsp.bundobust.controllers;
+
+public class EventDetailsUiRequest {
+	
+	private String eventId;
+	
+	private String eventName;
+	
+	private String eventDate;
+	
+	// TODO ADD the details sdpo, district , circle etc.
+	
+	
+
+}
